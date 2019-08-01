3c46e4f9ab5acb0c4c9d11124464a58a463cb32c
ColetonS <coleton.m.smith@gmail.com>
2019-08-01 17:05:49 -0600

    finished step 3

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -30,12 +30,26 @@ export default class Header extends Component {
 
   login() {
     // axios POST to /auth/login here
+    const {username, password} = this.state
+    axios
+      .post('/auth/login', {
+        username,
+        password
+      })
+      .then(res => {
+        this.props.updateUser(res.data)
+        this.setState({
+          username: '',
+          password: ''
+        })
+      })
+      .catch(err => alert(err.response.request.response))
   }
 
   register() {
     // axios POST to /auth/register here
     const { username, password, isAdmin } = this.state;
-    console.log('testing')
+    // console.log('testing')
     axios
       .post("/auth/register", {
         username,
