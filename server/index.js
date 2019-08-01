56747b0e180a7eb318450ffc1e422567f630d6a8
ColetonS <coleton.m.smith@gmail.com>
2019-08-01 17:16:52 -0600

    finished step 4

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(session({
 // ENDPOINTS
 app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout)
 
 
 app.listen(SERVER_PORT, () => console.log(`Feed me ${SERVER_PORT} bowls of ramen!`))
