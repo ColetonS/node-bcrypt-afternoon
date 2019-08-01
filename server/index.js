d392d95f3f48f741220fbb0b3d1f9c4ac872b47f
ColetonS <coleton.m.smith@gmail.com>
2019-08-01 16:39:19 -0600

    finished 3.1

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./authController')
+
+const app = express()
+
+const {SESSION_SECRET, CONNECTION_STRING} = process.env
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('db connected! yay!')
+})
+
+const SERVER_PORT = 4001
+
+// TOP LEVEL MIDDLEWARE
+app.use(express.json())
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+})
+)
+
+// ENDPOINTS
+app.post('/auth/register', authCtrl.register)
+app.post('/auth/login', authCtrl.login)
+
+
+app.listen(SERVER_PORT, () => console.log(`Feed me ${SERVER_PORT} bowls of ramen!`))
